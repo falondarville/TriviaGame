0b599da67062c4783dabfdab8213b5be41c2ed42
Falon Darville <falondarville@gmail.com>
2018-02-18 21:37:56 -0800

    add identifier

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,61 +10,71 @@ var questions = [{
 		question: "Who directed, wrote, produced, and starred in The Room?",
 		choices: ["Tommy Wiseau", "Clint Eastwood", "James Franco"],
 		answerIndex: 0,
-		identifier: "questionOne"
+		identifier: "questionOne",
+		selected: null
 	},
 	{
 		question: "What did Lisa do to Johnny?",
 		choices: ["Forgot his birthday", "Cheated on him with his best friend", "Hired an assassin to kill him"],
 		answerIndex: 1,
-		identifier: "questionTwo"
+		identifier: "questionTwo",
+		selected: null
 	},
 	{
 		question: "What is Johnny's famous line in the scene where he buys Lisa flowers?",
 		choices: ["Oh hi doggy", "There won't be another day like this", "How could you do this to me"],
 		answerIndex: 0,
-		identifier: "questionThree"
+		identifier: "questionThree",
+		selected: null
 	},
 	{
 		question: "Who jumps into bed with Johnny and Lisa?",
 		choices: ["Mark, Johnny's best friend", "Denny, Johnny's neighbor", "Peter, Johnny's psychologist friend"],
 		answerIndex: 1,
-		identifier: "questionFour"
+		identifier: "questionFour",
+		selected: null
 	},
 	{
 		question: "Who is Chris-R?",
 		choices: ["Johnny's lawyer and friend", "A drug dealer who Denny owes money to", "The DJ at Johnny's birthday party"],
 		answerIndex: 1,
-		identifier: "questionFive"
+		identifier: "questionFive",
+		selected: null
 	},
 	{
 		question: "What is the name of the memoir written by Greg Sestero that describes the making of The Room?",
 		choices: ["Red, Red Room", "The Making of The Room", "The Disaster Artist"],
 		answerIndex: 2,
-		identifier: "questionSix"
+		identifier: "questionSix",
+		selected: null
 	},
 	{
 		question: "Which duo produced and directed the film adaptation of Sestero's making-of memoir?",
 		choices: ["Patton Oswalt and Jon Hamm", "Greg Sestero and Dave Franco", "Seth Rogen and James Franco"],
 		answerIndex: 2,
-		identifier: "questionSeven"
+		identifier: "questionSeven",
+		selected: null
 	},
 	{
 		question: "What does Lisa's mother, Claudette, reveal to Lisa suddenly that she never mentions again?",
 		choices: ["She has breast cancer", "She is in love with Johnny", "She adopted Lisa as an infant"],
 		answerIndex: 0,
-		identifier: "questionEight"
+		identifier: "questionEight",
+		selected: null
 	},
 	{
 		question: "What do fans famously bring to limited movie screenings of The Room?",
 		choices: ["Tape recorders", "Spoons", "Roses"],
 		answerIndex: 1,
-		identifier: "questionNine"
+		identifier: "questionNine",
+		selected: null
 	},
 	{
 		question: "What is Johnny's favorite sport?",
 		choices: ["Football", "Soccer", "Hockey"],
 		answerIndex: 0,
-		identifier: "questionTen"
+		identifier: "questionTen",
+		selected: null
 	}];
 
 //call iteration function and pass through array argument
@@ -86,33 +96,60 @@ function iterateThroughQuestionsArray(questions) {
 };
 
 function displayButton(){
+
 	$("#submitButton").append("<input type='submit' value='submit' id='buttonStyle'>");
+
 };
 
 //append question
 function displayQuestions(questions) {
-	$(".question-div").append("<div class='question-styling'>" + questions.question + "</div>");
-};
 
-var choicesArray = [];
-var answersArray = [0, 1, 0, 1, 1, 2, 2, 0, 1, 0];
+	$(".question-div").append("<div class='question-styling'>" + questions.question + "</div>");
+
+};
 
 //append choices
 function displayChoices(choices, questionIndex) {
+
 	for(var y = 0; y < choices.length; y++) {
-	$(".question-div").append(`<input type='radio' name=${questions[questionIndex].identifier} class='options' value=${y}>${choices[y]}</input> <br />`);
+
+	$(".question-div").append(`<input type='radio' name=${questions[questionIndex].identifier} class='options' value=${y}>${choices[y]} </input> <br />`);
+
 	}
 };
 
 console.log(questions.identifier);
 
-//this function pushes the index of the value chosen to the choicesArray
-//ISSUE: pushing to array means that I will be able to compare arrays only if the user selects the questions in that specific order
+
+
+//upon clicking the options for each question, run the compareAnswers function and 
 $('.options').click(function() {
-    console.log($(this).val());
-    choicesArray.push($(this).val());
-    console.log(choicesArray);
+	//call function that checks answer index
+	//update "selected" value for question clicked. Take the value, set to "selected" instead of null
+	// questions[questionIndex].selected = this.value
+
+	console.log(questions.id);
+	// compareAnswers(this.value, this.id);
+
 });
+
+// function compareAnswers(choice, answer){
+// 	console.log(answer);
+// 	//record the index of the chosen answer for a given question
+// 	if (choice) {
+// 		if (choice === answer) {
+// 			correctAnswers ++;
+// 			console.log("Correct Answers: " + correctAnswers);
+// 		}
+// 		if (choice !== answer) {
+// 			wrongAnswers ++;
+// 			console.log("Wrong Answers: " + wrongAnswers);
+// 		} 
+// 	}
+// 	else {
+// 		incompleteAnswers++;
+// 	} 
+// };
 
 //function to compare choicesArray to answersArray
 //function to determine if all values have been inputted, and if not, output the amount of unanswered questions the user left
@@ -123,7 +160,10 @@ $("#submitButton").on("click", function(){
 	// printResults();
 });
 
-//function to print results
+//function to display quiz results
+
+
+
 
 // function printResults() {
 // 	$(".question-div").append("<p>Correct Answers : " + correctAnswers "</p> <br /> <p>Incorrect Answers : " + wrongAnswers + "</p> <br /> <p>Number of Questions Left Unanswered : " + incompleteAnswers + "</p>")
@@ -146,6 +186,5 @@ function timeQuiz() {
 function displayTime() {
 	$("#timeRemaining").html(secondsRemaining);
 };
-
 
 });
