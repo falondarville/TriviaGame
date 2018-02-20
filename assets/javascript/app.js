120cae56e77168cdb00c984fd4d229ac81d5626c
Falon Darville <falondarville@gmail.com>
2018-02-19 20:19:06 -0800

    added results logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,77 +10,67 @@ var questions = [{
 		question: "Who directed, wrote, produced, and starred in The Room?",
 		choices: ["Tommy Wiseau", "Clint Eastwood", "James Franco"],
 		answerIndex: 0,
-		identifier: "questionOne",
-		selected: null
+		identifier: "questionOne"
 	},
 	{
 		question: "What did Lisa do to Johnny?",
 		choices: ["Forgot his birthday", "Cheated on him with his best friend", "Hired an assassin to kill him"],
 		answerIndex: 1,
-		identifier: "questionTwo",
-		selected: null
+		identifier: "questionTwo"
 	},
 	{
 		question: "What is Johnny's famous line in the scene where he buys Lisa flowers?",
 		choices: ["Oh hi doggy", "There won't be another day like this", "How could you do this to me"],
 		answerIndex: 0,
-		identifier: "questionThree",
-		selected: null
+		identifier: "questionThree"
 	},
 	{
 		question: "Who jumps into bed with Johnny and Lisa?",
 		choices: ["Mark, Johnny's best friend", "Denny, Johnny's neighbor", "Peter, Johnny's psychologist friend"],
 		answerIndex: 1,
-		identifier: "questionFour",
-		selected: null
+		identifier: "questionFour"
 	},
 	{
 		question: "Who is Chris-R?",
 		choices: ["Johnny's lawyer and friend", "A drug dealer who Denny owes money to", "The DJ at Johnny's birthday party"],
 		answerIndex: 1,
-		identifier: "questionFive",
-		selected: null
+		identifier: "questionFive"
 	},
 	{
 		question: "What is the name of the memoir written by Greg Sestero that describes the making of The Room?",
 		choices: ["Red, Red Room", "The Making of The Room", "The Disaster Artist"],
 		answerIndex: 2,
-		identifier: "questionSix",
-		selected: null
+		identifier: "questionSix"
 	},
 	{
 		question: "Which duo produced and directed the film adaptation of Sestero's making-of memoir?",
 		choices: ["Patton Oswalt and Jon Hamm", "Greg Sestero and Dave Franco", "Seth Rogen and James Franco"],
 		answerIndex: 2,
-		identifier: "questionSeven",
-		selected: null
+		identifier: "questionSeven"
 	},
 	{
 		question: "What does Lisa's mother, Claudette, reveal to Lisa suddenly that she never mentions again?",
 		choices: ["She has breast cancer", "She is in love with Johnny", "She adopted Lisa as an infant"],
 		answerIndex: 0,
-		identifier: "questionEight",
-		selected: null
+		identifier: "questionEight"
 	},
 	{
 		question: "What do fans famously bring to limited movie screenings of The Room?",
 		choices: ["Tape recorders", "Spoons", "Roses"],
 		answerIndex: 1,
-		identifier: "questionNine",
-		selected: null
+		identifier: "questionNine"
 	},
 	{
 		question: "What is Johnny's favorite sport?",
 		choices: ["Football", "Soccer", "Hockey"],
 		answerIndex: 0,
-		identifier: "questionTen",
-		selected: null
+		identifier: "questionTen"
 	}];
 
 //call iteration function and pass through array argument
 iterateThroughQuestionsArray(questions);
 
-//Function to iterate through questions array
+//function to iterate through questions array
 function iterateThroughQuestionsArray(questions) {
 
 	for(var x = 0; x < questions.length; x++){
@@ -95,13 +85,14 @@ function iterateThroughQuestionsArray(questions) {
 	displayButton();
 };
 
+//append button to bottom of the form
 function displayButton(){
 
 	$("#submitButton").append("<input type='submit' value='submit' id='buttonStyle'>");
 
 };
 
-//append question
+//append questions
 function displayQuestions(questions) {
 
 	$(".question-div").append("<div class='question-styling'>" + questions.question + "</div>");
@@ -109,6 +100,7 @@ function displayQuestions(questions) {
 };
 
 //append choices
+//each question's choices have a different name attribute so that users can only choose one response to each question
 function displayChoices(choices, questionIndex) {
 
 	for(var y = 0; y < choices.length; y++) {
@@ -118,51 +110,29 @@ function displayChoices(choices, questionIndex) {
 	}
 };
 
-console.log(questions.identifier);
-
-function setSelected(answer) {
-
-	//set two variables, one for answer.name and answer.value
-	//use the find function to get the object
-	//assign the value to the question
-	console.log(answer.target.name);
-	console.log(answer.target.value);
-};
-
-//upon clicking the options for each question, run the compareAnswers function and 
-$('.options').click(setSelected);
-
-// function compareAnswers(choice, answer){
-// 	console.log(answer);
-// 	//record the index of the chosen answer for a given question
-// 	if (choice) {
-// 		if (choice === answer) {
-// 			correctAnswers ++;
-// 			console.log("Correct Answers: " + correctAnswers);
-// 		}
-// 		if (choice !== answer) {
-// 			wrongAnswers ++;
-// 			console.log("Wrong Answers: " + wrongAnswers);
-// 		} 
-// 	}
-// 	else {
-// 		incompleteAnswers++;
-// 	} 
-// };
-
-//function to compare choicesArray to answersArray
-//function to determine if all values have been inputted, and if not, output the amount of unanswered questions the user left
 $("#submitButton").on("click", function(){
-	//submit form 
-	alert("button works!");
-	//call two function
+
+	calculateScore();
 	// printResults();
 });
 
-//function to display quiz results
+function calculateScore() {
 
+	$.each(questions, function(key, value) {
 
+		var valueOf = $(`input[name=${value.identifier}]:checked`).val()
 
+		if (valueOf == value.answerIndex) {
+			correctAnswers++;
+		} else if (valueOf == undefined) {
+			incompleteAnswers++;
+		} else if (valueOf != value.answerIndex) {
+			wrongAnswers++;
+		}
+
+	});
+	alert ("correct = " + correctAnswers + "wrong = " + wrongAnswers + "incompleteAnswers =" + incompleteAnswers);
+};
 
 // function printResults() {
 // 	$(".question-div").append("<p>Correct Answers : " + correctAnswers "</p> <br /> <p>Incorrect Answers : " + wrongAnswers + "</p> <br /> <p>Number of Questions Left Unanswered : " + incompleteAnswers + "</p>")
@@ -177,9 +147,13 @@ function timeQuiz() {
 	displayTime();
 	if (secondsRemaining !== 0) {
 		secondsRemaining -= 1;
-	//call function that displays quiz results
-	
+
 	}
+	// if (secondsRemaining == 0) {
+	// //call function that displays quiz results
+	// // calculateScore();
+	// // printResults();
+	// }
 };
 
 function displayTime() {
